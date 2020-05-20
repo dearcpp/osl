d5b9ccda0ff562980a5b2e315460ed4d6ff11c38
beryll1um <vitalii.babichuk@gmail.com>
2020-05-20 09:42:30 +0300

    impl(memory_copy): own memcpy implemenented

--- example/allocator/allocator.cpp
+++ example/allocator/allocator.cpp
@@ -3,6 +3,7 @@
 
 int main() {
     osl::allocator<int> alloc(5);
+    alloc.reallocate(10);
     for (size_t i = 0; i < alloc.allocated(); i++) {
         alloc[i] = (int)i;
         printf("value: %i\n", alloc[i]);
