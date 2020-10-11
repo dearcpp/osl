70c4f4eb74dc659d0f997d985ef77d08cebd7321
beryll1um <vitalii.babichuk@gmail.com>
2020-10-11 23:20:24 +0300

    fix(cmake): warning level was reduced

--- libs/string.hpp
+++ libs/string.hpp
@@ -25,9 +25,10 @@ namespace osl
             this->allocate(_length + 1);
             memory_copy<type>(this->_pointer, pointer, _length);
             this->_pointer[_length] = 0;
-        }
+        }s
 
-        basic_string(basic_string const &object) : _length(object.length()), allocator<_type>(object.length() + 1) {
+        basic_string(basic_string const &object) : _length(object.length()) {
+            this->allocate(_length + 1);
             memory_copy<type>(this->_pointer, object.c_str(), _length);
             this->_pointer[_length] = 0;
         }
