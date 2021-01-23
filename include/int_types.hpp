a20f430f0d3350c0a78aeea33efde901d4042f28
beryll1um <vitalii.babichuk@gmail.com>
2021-01-23 22:13:36 +0200

    fix(int_types): non-cross-platform header removed

--- include/int_types.hpp
+++ include/int_types.hpp
@@ -2,29 +2,18 @@
 #define _OSL_TYPES_HPP
 
 # include <defines.hpp>
-# include <bits/wordsize.h>
 
 OSL_BEGIN_NAMESPACE
 
 typedef signed char s8;
 typedef short int   s16;
 typedef int         s32;
-
-# if __WORDSIZE == 64
- typedef long int s64;
-# else
- typedef long long int s64;
-# endif
+typedef long long   s64;
 
 typedef unsigned char      u8;
 typedef unsigned short int u16;
 typedef unsigned int       u32;
-
-#if __WORDSIZE == 64
- typedef unsigned long int u64;
-#else
- typedef unsigned long long int u64;
-#endif
+typedef unsigned long long u64;
 
 OSL_END_NAMESPACE
 
