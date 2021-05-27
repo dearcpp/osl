36b415a60b01938cdd38b1233f1ed8925f9f5ab0
beryll1um <beryll1um.d3v@gmail.com>
2021-05-27 10:03:03 +0300

    fix(queue): queue was modified (FIFO)

--- examples/queue/queue.cpp
+++ examples/queue/queue.cpp
@@ -5,13 +5,15 @@
 using namespace osl;
 
 int main(int, char**) {
-    queue<int> queue_1;
-    for (u32 i = 0; i < 10; ++i)
-        queue_1.push(i);
+    queue<int> queue;
 
-    queue<int> queue_2 = queue_1;
     for (u32 i = 0; i < 10; ++i)
-        printf("%i\n", queue_2.top()), queue_2.pop();
+        queue.push(i);
+
+    for (u32 i = 0; i < 10; ++i) {
+        printf("%i\n", queue.back());
+        queue.pop();
+    }
 
     return 0;
 }
